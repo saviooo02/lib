ff24897488fb1ada3223357f120cc2a80cdbd007
saviooo02 <savioshaji02@gmail.com>
2026-03-02 17:09:32 +0530

    feat: add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
   return (
-    <div>
-      <h1>App</h1>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
