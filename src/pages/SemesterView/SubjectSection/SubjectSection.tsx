7600079778d28740d6195f0e788c971bdce92b2f
saviooo02 <savioshaji02@gmail.com>
2026-03-03 12:10:57 +0530

    feat: add subject button

--- src/pages/SemesterView/SubjectSection/SubjectSection.tsx
+++ src/pages/SemesterView/SubjectSection/SubjectSection.tsx
@@ -1,5 +1,7 @@
 import SubjectCard from "./SubjectCard";
 import { FlaskConical } from "lucide-react";
+import { Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
 export const SubjectSection = () => {
   return (
     <div
@@ -21,6 +23,35 @@ export const SubjectSection = () => {
           onViewClick={() => console.log("View clicked")}
         />
       ))}
+      <div className="w-full max-w-sm">
+        <button
+          className="
+      group w-full h-full
+      rounded-2xl border-2 border-dashed
+      border-muted-foreground/30
+      bg-muted/40
+      p-6
+      flex flex-col items-center justify-center gap-3
+      transition-all duration-200
+      hover:border-gray-400
+      hover:bg-muted
+      hover:shadow-md
+    "
+          onClick={() => console.log("Add subject clicked")}
+        >
+          <div className="p-3 rounded-full bg-background border border-primary transition">
+            <Plus className="h-5 w-5  text-primary transition" />
+          </div>
+
+          <div className="text-sm font-medium text-foreground group-hover:text-foreground transition">
+            Add New Subject
+          </div>
+
+          <p className="text-xs text-muted-foreground/70 text-center">
+            Create a new subject and start adding materials
+          </p>
+        </button>
+      </div>
     </div>
   );
 };
